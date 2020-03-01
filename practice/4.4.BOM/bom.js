56560079b8353685481e5785fee88b593e52e21e
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-03-01 15:05:09 +0100

    0103201505

--- practice/4.4.BOM/bom.js
+++ practice/4.4.BOM/bom.js
@@ -1,88 +1,161 @@
-/****   window.navigator    ****/
+/****   using JS on webpage  ****/
 
-// function nav() {
+// function trigger() {
+//     console.log("Nenad Dimitrijevic");
+// }
+// trigger();
+
+/****   BOM (Window object)  ****/
+
+//window.navigator
+
+// function info() {
 //     console.log(window.navigator.platform);
 //     console.log(window.navigator.appVersion);
 //     console.log(window.navigator.appName);
 // }
-// nav();
 
-// function isOnline(int) {
-//     if (int) {
+// function isOnline(input) {
+//     if (input) {
 //         console.log("Online");
 //     } else {
-//         console.log("Offline")
+//         console.log("Offline");
 //     }
 // }
+// info();
 // isOnline(window.navigator.onLine);
 
-/****    window.screen   ****/
+//window.screen
 
-// function widthHeight() {
-//     console.log(window.screen.availWidth);
-//     console.log(window.screen.availHeight);
-//     console.log(window.screen.height);
-
+// function widthHeigth() {
+//     console.log("current width: " + window.screen.availWidth);
+//     console.log("current height: " + window.screen.availHeight);
+//     console.log("max width: " + window.screen.width);
+//     console.log("max height: " + window.screen.height);
 // }
+// widthHeigth();
 
-// widthHeight();
+//window.location
 
-/****    window.location   ****/
-
-// function website() {
+// function printInfo() {
 //     console.log(window.location.href);
-//     console.log(window.location.hostname);
+//     console.log(window.location.host);
 //     console.log(window.location.protocol);
 //     console.log(window.location.search);
 // }
 
-
-
-// function rel() {
-//     location.reload();
+// function reloading() {
+//     window.location.reload();
 // }
 
-// function changePage() {
-//     location.href = "https://www.bgit.rs/";
+// function goTo(input) {
+//     window.location.href = "https://github.com/";
 // }
 
-/****     window.history     ****/
+//window.localStorage
 
-// history.back();
-// history.forward();
+// function setInfo(a, b) {
+//     window.localStorage.setItem(a, b);
+// }
 
-/****   local.storage   ****/
-
-function storeData(a, b) {
-    window.localStorage.setItem(a, b)
-}
-
-//storeData("age", "32");
-
-function readData() {
-    var age = localStorage.getItem("age");
-
-    if (age) {
-        console.log(age)
-    } else {
-        console.log("There is no data")
-    }
-}
-
-readData();
-
-function clearData(a) {
-    window.localStorage.removeItem(a);
-}
-
-/****    window methods   ****/
-
-// function greeting() {
-//     alert("hello");
-//     var answer = prompt("how old are you?");
-//     if (answer) {
-//         confirm("We will submit this answer " + answer)
+// function getInfo(a) {
+//     var data = window.localStorage.getItem(a);
+//     if (data) {
+//         console.log(data);
+//     } else {
+//         console.log("There is no data!");
 //     }
-//     console.log("Hirurgija");
 // }
-// greeting();
+
+// function removeInfo(a) {
+//     window.localStorage.removeItem(a);
+// }
+
+//window.sessionStorage
+
+// function setSessInfo(a, b) {
+//     window.sessionStorage.setItem(a, b);
+// }
+
+// function getSessInfo(a) {
+//     var data = window.sessionStorage.getItem(a);
+//     if (data) {
+//         console.log(data);
+//     } else {
+//         console.log("We do not know that");
+//     }
+// }
+
+// function removeSessInfo(a) {
+//     window.sessionStorage.removeItem(a);
+// }
+
+//window.history
+
+// window.history.forward();
+
+// window.history.back();
+
+/*********   Window Methods   ********/
+
+// function greetings() {
+//     alert("Welcome here!");
+//     var answer = prompt("What is your name?");
+//     var answer2 = confirm("We will submit this answer now! " + answer);
+//     if (answer2) {
+//         alert("Success!");
+//     }
+// }
+// greetings();
+
+/********   Global JavaScript Objects (homework)  ******/
+
+//Math
+
+// function randomNumbers() {
+//     var arr = [];
+//     for (var i = 0; i < 10; i++) {
+//         arr.push(Math.random() * 10);
+//         //console.log(arr[i]);
+//     }
+//     return arr;
+// }
+// var arrayRandom = randomNumbers();
+
+// function roundNumbers(arr) {
+//     var newArr = [];
+//     for (var i = 0; i < arr.length; i++) {
+//         newArr.push(arr[i].toFixed(2));
+//         //console.log(newArr[i]);
+//     }
+//     return newArr;
+// }
+// var arrayRound = roundNumbers(arrayRandom);
+
+// function floorNumbers(arr) {
+//     var newArr = [];
+//     for (var i = 0; i < arr.length; i++) {
+//         newArr.push(Math.floor(arr[i]));
+//         //console.log(newArr[i]);
+//     }
+//     return newArr;
+// }
+// var arrayFloor = floorNumbers(arrayRound);
+
+// function maxNumber(arr) {
+//     console.log(arr);
+//     console.log(Math.max.apply(null, arr));
+// }
+// maxNumber(arrayFloor);
+
+//Date
+
+// function printWholeDate() {
+//     var a = new Date();
+//     console.log(a);
+//     var b = a.getHours() + ":" + a.getMinutes();
+//     console.log(b);
+//     var c = a.getDate() + "." + (a.getMonth() + 1) + "." + a.getFullYear() + ".";
+//     console.log(c);
+// }
+// printWholeDate();
