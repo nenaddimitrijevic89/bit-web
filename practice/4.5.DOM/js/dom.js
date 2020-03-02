187faf68f47295b409d5f39c595066f8cef60170
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-03-03 00:19:05 +0100

    0303200018

--- practice/4.5.DOM/js/dom.js
+++ practice/4.5.DOM/js/dom.js
@@ -73,10 +73,9 @@
 //     parent.removeChild(child);
 // }
 
-
 /******    Attributes   *******/
 
-// var createForm = function (type, label, req) {
+// var createForm = function(type, label, req) {
 //     var form = document.querySelector("form");
 
 //     var inputEl = document.createElement("input");
@@ -88,16 +87,13 @@
 //     if (req) {
 //         inputEl.setAttribute("required", "required");
 //     }
-//     form.appendChild(labelEl)
+//     form.appendChild(labelEl);
 //     form.appendChild(inputEl);
-// }
+// };
 
 // createForm("text", "name", true);
 // createForm("password", "password");
-// createForm("submit", "submit")
-
-
-
+// createForm("submit", "submit");
 
 // var a = ["nature1.jpg", "nature2.jpg", "nature3.jpg"];
 // var div = document.querySelector("#third-div");
@@ -105,14 +101,14 @@
 // function addImages(arr, el) {
 //     for (var i = 0; i < arr.length; i++) {
 //         var img = document.createElement("img");
-//         img.setAttribute("src", './images/' + arr[i]);
+//         img.setAttribute("src", "./images/" + arr[i]);
 //         el.appendChild(img);
 //     }
 //     document.querySelector("div img").classList.add("active");
 // }
 
 // function slide() {
-//     var active = document.querySelector('.active');
+//     var active = document.querySelector(".active");
 //     active.classList.remove("active");
 
 //     if (active.nextElementSibling) {
@@ -124,3 +120,105 @@
 
 // addImages(a, div);
 // setInterval(slide, 1000);
+
+/***************************************************************************/
+
+// var body = document.querySelector("body");
+
+// var createForm = function() {
+//     var form = document.createElement("form");
+//     body.appendChild(form);
+// };
+// createForm();
+
+// var form = document.querySelector("form");
+
+// var inputLabel = function(type, name, req) {
+//     var input = document.createElement("input");
+//     var label = document.createElement("label");
+//     var text = document.createTextNode(name);
+//     input.setAttribute("type", type);
+//     label.appendChild(text);
+//     if (req) {
+//         input.setAttribute("required", "required");
+//     }
+//     if (typeof name === "undefined") {
+//         name = " ";
+//     }
+//     form.appendChild(label);
+//     form.appendChild(input);
+// };
+// inputLabel("text", "Name", true);
+// inputLabel("password", "Password", true);
+// inputLabel("number", "Age", false);
+// inputLabel("submit", " ");
+
+// /**********************************************/
+
+// var a = ["nature1.jpg", "nature2.jpg", "nature3.jpg"];
+
+// var createDiv = function() {
+//     var div = document.createElement("div");
+//     body.appendChild(div);
+//     div.className = "img-div";
+// };
+// createDiv();
+
+// var div = document.querySelector(".img-div");
+
+// var createImg = function(arr) {
+//     for (var i = 0; i < arr.length; i++) {
+//         var img = document.createElement("img");
+//         img.setAttribute("src", "./images/" + arr[i]);
+//         div.appendChild(img);
+//     }
+//     document.querySelector("div img").classList.add("active");
+// };
+// createImg(a);
+
+// var slideImg = function() {
+//     var active = document.querySelector(".active");
+//     active.classList.remove("active");
+
+//     if (active.nextElementSibling) {
+//         active.nextElementSibling.classList.add("active");
+//     } else {
+//         document.querySelector("div img").classList.add("active");
+//     }
+// };
+
+// setInterval(slideImg, 3000);
+
+/******************     slike    ********************/
+
+var a = ["nature1.jpg", "nature2.jpg", "nature3.jpg"];
+var b = ["madman.jpg", "gorila.png", "pizza.gif"];
+
+var div = document.querySelector("#third-div");
+
+var imageCreate = function(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        var img = document.createElement("img");
+        img.setAttribute("src", "./images/" + arr[i]);
+        div.appendChild(img);
+    }
+};
+imageCreate(a);
+imageCreate(b);
+
+var changeImgPosition = function() {
+    var image = document.querySelector("#third-div img");
+    div.appendChild(image);
+};
+setInterval(changeImgPosition, 3000);
+
+// var changeImgPosition = function() {
+//     var image = document.querySelector("#third-div img");
+//     image.classList.add("img-none");
+
+//     setInterval(function() {
+//         div.appendChild(image);
+//         image.classList.remove("img-none");
+//     }, 3000);
+// };
+// setInterval(changeImgPosition, 3000);
