6b71e975afc6b98d784f2092f5d24e6e8923dd82
BIT Student <classroom@bgit.rs>
2020-03-03 18:04:59 +0100

    0303201804

---
+++ practice/5.1.events/js/events.js
@@ -0,0 +1,38 @@
+
+// function changeBack() {
+//     var div = document.querySelector("#first-div");
+//     div.classList.toggle("blue");
+// }
+// function turnOff() {
+//     var div = document.querySelector("#first-div");
+//     div.classList.remove("blue");
+// }
+
+
+// function send() {
+//     var input = document.querySelector("#input");
+//     var cont = document.querySelector("#second-div");
+//     var paragraph = document.createElement("p");
+//     var text = document.createTextNode(input.value);
+//     paragraph.appendChild(text)
+//     cont.appendChild(paragraph);
+//     input.value = "";
+// }
+// send();
+
+
+
+
+
+
+// var movePlayer = function () {
+//     var a = document.querySelector("body");
+//     var player = document.querySelector("img");
+
+//     a.addEventListener("click", function (event) {
+//         player.style.top = event.clientY + "px";
+//         player.style.left = event.clientX + "px";
+//     })
+// }
+
+// movePlayer();
