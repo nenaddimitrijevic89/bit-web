49d9db1d3b066d8028eacf2fe80977e7118a17da
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-03-03 22:38:32 +0100

    0303202238

--- practice/5.1. events/js/events.js
+++ practice/5.1. events/js/events.js
@@ -1,38 +1,39 @@
+/******     Toggle background    ******/
 
 // function changeBack() {
 //     var div = document.querySelector("#first-div");
 //     div.classList.toggle("blue");
 // }
+
 // function turnOff() {
 //     var div = document.querySelector("#first-div");
 //     div.classList.remove("blue");
+//     div.classList.toggle("orange");
 // }
 
+/********    Chat    *******/
 
 // function send() {
 //     var input = document.querySelector("#input");
 //     var cont = document.querySelector("#second-div");
 //     var paragraph = document.createElement("p");
 //     var text = document.createTextNode(input.value);
-//     paragraph.appendChild(text)
+//     paragraph.appendChild(text);
 //     cont.appendChild(paragraph);
 //     input.value = "";
 // }
 // send();
 
+/**********     Football    ********/
 
-
-
-
-
-// var movePlayer = function () {
+// var movePlayer = function() {
 //     var a = document.querySelector("body");
 //     var player = document.querySelector("img");
 
-//     a.addEventListener("click", function (event) {
+//     a.addEventListener("click", function(event) {
 //         player.style.top = event.clientY + "px";
 //         player.style.left = event.clientX + "px";
-//     })
-// }
+//     });
+// };
 
 // movePlayer();
