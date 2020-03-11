ee1609352793b06f22583c5ee111c355a4e6f8be
Nenad Dimitrijevic <nenaddimitrijevic89@gmail.com>
2020-03-11 21:33:16 +0100

    1103202133

--- practice/6.1. ajax/gitHubSearcher/js/gitHubSearch.js
+++ practice/6.1. ajax/gitHubSearcher/js/gitHubSearch.js
@@ -3,10 +3,11 @@ var input = document.querySelector(".form-control");
 var button = document.querySelector(".dugme");
 var body = document.querySelector("body");
 var div = document.querySelector(".profile")
+var section = document.querySelector("section");
 
 button.onclick = function () {
-
-
+    section.innerHTML = "";
+    div.innerHTML = "";
     var request = new XMLHttpRequest();
     request.open("GET", "https://api.github.com/search/users?q=" + input.value);
     request.send();
@@ -19,40 +20,40 @@ button.onclick = function () {
             img.setAttribute("src", data.items[i].avatar_url);
             var name = document.createElement("h3");
             name.textContent = data.items[i].login;
-            var div1 = document.createElement("div");
-            div1.className = "profiles col";
-            div1.appendChild(img);
-            div1.appendChild(name);
-            div.appendChild(div1);
+            var profile = document.createElement("div");
+            profile.className = "profiles col";
+            profile.appendChild(img);
+            profile.appendChild(name);
+            div.appendChild(profile);
             input.value = "";
-
         }
         addClick();
     }
-
 }
 function addClick() {
 
-    console.log("bla");
-
     $(".profiles").on("click", function (event) {
-        var user = event.currentTarget.querySelector("h3").textContent;
-        console.log(user);
 
+        var user = event.currentTarget.querySelector("h3").textContent;
 
         var request1 = new XMLHttpRequest();
         request1.open("GET", "https://api.github.com/users/" + user + "/repos");
         request1.send();
 
         request1.onload = function () {
+
             var data1 = JSON.parse(request1.responseText);
-            console.log(data1);
-            var section = document.querySelector("section");
             section.innerHTML = "";
             for (var i = 0; i < data1.length; i++) {
-                var divRep = document.createElement("div");
-                divRep.textContent = data1[i].name;
-                section.appendChild(divRep);
+                var divRepository = document.createElement("div");
+                divRepository.className = "repository";
+                var h4 = document.createElement("h4");
+                var p = document.createElement("p");
+                p.textContent = data1[i].language;
+                h4.textContent = data1[i].name;
+                divRepository.appendChild(h4);
+                divRepository.appendChild(p);
+                section.appendChild(divRepository);
             }
         }
     })
